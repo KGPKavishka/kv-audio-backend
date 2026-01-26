9334366f1f5b9aa1eadd8aceea512919100cc2e2
Pasindu Kavishka <pasindukv29@gmail.com>
2026-01-27 00:00:18 +0530

    Product management part is done and Inquiiry.js file created 😮‍💨

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,7 @@
 import Product from "../models/product.js";
+import { isItAdmin } from "./userController.js";
 
 export async function addProduct(req, res) {
-
-    console.log(req.user);
 
     if(req.user == null){
         res.status(401).json({ 
@@ -38,4 +37,73 @@ export async function addProduct(req, res) {
     // .catch((error) => {
     //     res.status(500).json({ error: "Error adding product" });
     // });
+}
+
+export async function getProducts(req, res) {
+
+    try {
+        if(isItAdmin(req)){
+            const products = await Product.find();
+            res.json(products);
+            return;
+        }else{
+            const products = await Product.find({ availability: true });
+            res.json(products);
+            return;
+        }
+        
+
+    } catch (error) {
+        res.status(500).json({ 
+            message: "Error fetching products" 
+        });
+    }
+}
+
+export async function updateProduct(req, res) {
+    try {
+        if(isItAdmin(req)){
+            const key = req.params.key;
+            const data = req.body;
+            await Product.updateOne({ key:key}, data)
+
+            res.json({
+                message: "Product updated successfully"
+            })
+            return;
+
+        }else{
+            res.status(403).json({
+                message: "Access denied. Admins only"
+            })
+            return;
+        }
+
+    }catch (error) {
+        res.status(500).json({
+            message: "Error updating product"
+        })
+    }
+}
+
+export async function deleteProduct(req, res) {
+    try{
+        if(isItAdmin(req)){
+            const key = req.params.key;
+            await Product.deleteOne({ key : key});
+            res.json({
+                message: "Product deleted successfully"
+            })
+        }else{
+            res.status(403).json({
+                message: "Access denied. Admins only"
+            });
+            return;
+        }
+
+    }catch(error){
+        res.status(500).json({
+            message: "Error deleting product"
+        })
+    }
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -51,3 +51,21 @@ export function loginUser (req,res){
     )
 
 }
+
+
+export function isItAdmin(req){
+    let isAdmin = false;
+    if(req.user != null && req.user.role == "admin"){
+        isAdmin = true;
+    }
+
+    return isAdmin;
+}
+
+export function isItCustomer(req){
+    let isCustomer = false;
+    if(req.user != null && req.user.role == "customer"){
+        isCustomer = true;
+    }
+    return isCustomer;
+}
